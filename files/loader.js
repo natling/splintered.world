a5f4bf560b8eca2f46502eea25ca30827dae887c
Natalie Braginsky <hi@natalie.computer>
2019-02-09 15:01:33 -0500

    add loader

---
+++ files/loader.js
@@ -0,0 +1,7 @@
+const container = document.createElement('div');
+document.body.appendChild(container);
+container.id = 'container';
+
+const loader = document.createElement('div');
+container.appendChild(loader);
+loader.id = 'loader';
